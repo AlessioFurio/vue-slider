defc62c9d279bae07d34eeb51e598c23ecdf319f
Furio Alessio <alefurio@hotmail.it>
2020-11-09 16:05:12 +0100

    aggiunta set interval di 2 sec allo slider

--- main.js
+++ main.js
@@ -14,19 +14,27 @@ var app = new Vue({
         ]
     },
     methods: {
-        nextImage () { // funzione incremento per scorrere in avanti lo slider
-            this.imageIndex += 1; // incremento di 1 l' indice corrente
-            if(this.imageIndex >= this.images.length){
-                this.imageIndex = 0; // se l' indice supera images.length, lo porto a 0
-            }
-        },
+        nextImage() {
+            setInterval(() => {
+
+                this.imageIndex += 1; // incremento di 1 l' indice corrente
+                if(this.imageIndex >= this.images.length){
+                    this.imageIndex = 0; // se l' indice supera images.length, lo porto a 0
+                }
+            }, 2000) // fine set interval
+        }, // fine nextImage
+
         prevImage () { // funzione decremento per scorrere all' indietro lo slider
             this.imageIndex -=1; // decremento di 1 indice corrente
             if (this.imageIndex < 0) {
                 this.imageIndex = this.images.length -1; //se l' indice e' minore di 0, gli assegno il valore di images.length - 1 per farlo ripartire dall' ultimo elemento presente nell' array
             }
-        }
-    }
+        }, // fine prevImage
 
+    }, // fine methods
 
-});
+    created() {
+    this.nextImage()
+    }, // fine created
+
+}); // fine app
