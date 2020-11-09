a9bd6558923d94ccdbf8431c6ce03ce429156b92
Furio Alessio <alefurio@hotmail.it>
2020-11-09 14:58:24 +0100

    slider con vue js

---
+++ main.js
@@ -0,0 +1,32 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS: Aggiungere un’immagine presa anch’essa da un data
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        imageIndex: 0,
+        images: [
+            "images/image1.jpg",
+            "images/image2.jpg",
+            "images/image3.jpg",
+            "images/image4.jpg",
+            "images/image5.jpg"
+        ]
+    },
+    methods: {
+        nextImage () { // funzione incremento per scorrere in avanti lo slider
+            this.imageIndex += 1; // incremento di 1 l' indice corrente
+            if(this.imageIndex >= this.images.length){
+                this.imageIndex = 0; // se l' indice supera images.length, lo porto a 0
+            }
+        },
+        prevImage () { // funzione decremento per scorrere all' indietro lo slider
+            this.imageIndex -=1; // decremento di 1 indice corrente
+            if (this.imageIndex < 0) {
+                this.imageIndex = this.images.length -1; //se l' indice e' minore di 0, gli assegno il valore di images.length - 1 per farlo ripartire dall' ultimo elemento presente nell' array
+            }
+        }
+    }
+
+
+});
